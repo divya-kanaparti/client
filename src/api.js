ca110c03d65860920c8c3f23053b50316b402d24
divya-kanaparti <divyakanaparti@gmail.com>
2025-10-31 16:28:22 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,35 @@
+// // import axios from 'axios';
+
+// // const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+// // const api = axios.create({ baseURL: API_BASE });
+
+// // export const setAuthToken = (token) => {
+// //   if (token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+// //   else delete api.defaults.headers.common['Authorization'];
+// // };
+
+// // export default api;
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api", // 👈 backend server URL
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
+import axios from "axios";
+
+// ✅ Use environment variable (from .env) OR fallback to localhost
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+const api = axios.create({
+  baseURL: API_BASE + "/api",  // ensures all calls go to /api/*
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
